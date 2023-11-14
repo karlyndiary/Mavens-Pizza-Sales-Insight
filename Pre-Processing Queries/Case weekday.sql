535f2abc727c919f294e19f9bbb535b9f2fabbc6
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2023-11-14 08:10:10 +0530

    Update Case weekday.sql

--- Pre-Processing Queries/Case weekday.sql
+++ Pre-Processing Queries/Case weekday.sql
@@ -1,8 +1,8 @@
-
 -- Assuming you have a table called "my_table" with a date column called "my_date" and a column called "weekday" that needs to be updated
 -- USE pizza.dbo.orders
 
 -- Update "weekday" column based on "my_date" column
+
 UPDATE pizza.dbo.orders
 SET weekday = CASE 
     WHEN DATEPART(weekday, date) = 1 THEN 'Sunday'
