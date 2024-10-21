263b990982fe08c67969dc91e0d2a5b95b601088
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-10-21 16:24:29 +0530

    Update [06] Peak Days.sql

--- Queries & Insight/[06] Peak Days.sql
+++ Queries & Insight/[06] Peak Days.sql
@@ -1,8 +1,9 @@
 -- Are there any peak days?
 
-SELECT count(order_id) as orders, weekday
-FROM pizza.dbo.orders
-GROUP BY weekday 
+SELECT COUNT(o.order_id) AS orders, DATENAME(weekday, o.date) AS weekday
+FROM pizza.dbo.orders o
+GROUP BY DATENAME(weekday, o.date) 
 ORDER BY orders DESC;
+
 
 Friday, Thursday, and Saturday are the peak days.
