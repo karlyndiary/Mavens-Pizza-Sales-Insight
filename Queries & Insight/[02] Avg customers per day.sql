3b01b1c373bdda2a0421be17ac0fe3b402cd3901
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-10-22 12:06:52 +0530

    Update [02] Avg customers per day.sql

--- Queries & Insight/[02] Avg customers per day.sql
+++ Queries & Insight/[02] Avg customers per day.sql
@@ -1,6 +1,6 @@
 -- What is the average number of customers per day?
 
-SELECT count(order_id)/count(distinct date) as average_customers_per_day
+SELECT count(distinct order_id)/count(date) as average_customers_per_day
 FROM pizza.dbo.orders;
 
 The average daily customer traffic is 59.
