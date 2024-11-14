d40dea94996d037a18cc16af85376ea90fd74e5b
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-11-14 17:34:39 +0530

    Update [07] Month with most orders.sql

--- Queries & Insight/[07] Month with most orders.sql
+++ Queries & Insight/[07] Month with most orders.sql
@@ -1,9 +1,9 @@
 -- Which month experienced the highest number of orders?
 
-SELECT DATENAME(month, o.date) as month,  as revenue count(o.order_id) as orders
-FROM [Pizza].[dbo].[order_details] od
-JOIN [Pizza].[dbo].[orders] o
-ON od.order_id = o.order_id
+SELECT DATENAME(month, o.date) as month, count(o.order_id) as orders
+FROM pizza.dbo.orders o
+JOIN pizza.dbo.order_details od 
+ON o.order_id = od.order_id
 GROUP BY DATENAME(month, o.date) 
 ORDER BY orders DESC;
 
